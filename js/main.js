f1c69e3c89aa3fe32510d5a9bdaf111fe7d82a2a
Max <max.kuzn.msx@gmail.com>
2022-07-19 17:36:14 +0300

    all without modal and form

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
+$("body").on('click', '[href*="#"]', function (e) {
+    var fixed_offset = 50;
+    $('html,body').stop().animate({ scrollTop: $(this.hash).offset().top - fixed_offset }, 1000);
+    e.preventDefault();
+});
 $(".reviews__slider").slick({
     slide: ".reviews__block",
     slidesToShow: 2,
@@ -7,7 +12,14 @@ $(".reviews__slider").slick({
     autoplay: true,
     autoplaySpeed: 225000,
     prevArrow: ".reviews__prev",
-    nextArrow: ".reviews__next"
+    nextArrow: ".reviews__next",
+    responsive: [{
+        breakpoint: 576,
+        settings: {
+            slidesToShow: 1,
+            vertical: false
+        }
+    }]
 });
 if (screen.width >= 1200) {
     let bg = $('.main__img .after');
@@ -19,3 +31,11 @@ if (screen.width >= 1200) {
         bg2.css('transform', 'translate(-' + x * 30 + 'px, -' + y * 30 + 'px)');
     });
 }
+$('.header__burger').on('click', function (e) {
+    e.preventDefault();
+    $('.header__drop').addClass('active');
+});
+$('.header__close, .header__menu a').on('click', function (e) {
+    e.preventDefault();
+    $('.header__drop').removeClass('active');
+});
